1b6e15bf6c78738995af6ca980882466e079f87c
Kyle Cuevas <kyle.automation.test@gmail.com>
2024-04-22 02:58:09 -0400

    added window and tabs activity

--- tests/basicActions.test.ts
+++ tests/basicActions.test.ts
@@ -1,4 +1,4 @@
-import {expect, test} from "@playwright/test";
+import {expect, Page, test} from "@playwright/test";
 
 
 test("simple form demo", async({ page }) => {
@@ -162,3 +162,46 @@ test("frames demo", async({ page }) => {
 
     await page.waitForTimeout(3000);
 })
+
+test.only("window and tabs demo", async({ page }) => {
+    await page.goto("https://www.lambdatest.com/selenium-playground/window-popup-modal-demo");
+
+    const [multiPage] = await Promise.all([
+        page.waitForEvent("popup"),
+        page.click("#followboth")
+    ]);
+
+    await multiPage.waitForLoadState('load');
+
+    const pages = multiPage.context().pages();
+    console.log("No. of tabs: " + pages.length);
+
+    pages.forEach(tab => {
+        console.log(tab.url())
+    });
+
+    /* assigning tab
+    let facebookPage: Page;
+    for (let index = 0; index < pages.length; index++) {
+        const url = pages[index].url()
+        if (url == "https://www.facebook.com/lambdatest/") {
+            facebookPage = pages[index]
+        }
+    }
+
+    const text = await facebookPage.textContent("//h1");
+    console.log(text);
+    */
+
+    // await pages[1].fill("","") //interact with specific tab
+
+    /* singleWindow
+    const [newWindow] = await Promise.all([
+        page.waitForEvent("popup"),
+        page.click("//a[contains(text(),'Follow On Twitter')]")
+    ]);
+
+    console.log(newWindow.url());
+    // newWindow.fill("",""); //interact with new window
+    */
+});
